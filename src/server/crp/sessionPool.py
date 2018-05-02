449ff9063d1e919127448fd735d744f0215606b4
asir <asirlu@foxmail.com>
2018-05-02 22:20:21 +0800

    拒绝同一个微信用户建立多个会话的请求

--- src/server/crp/sessionPool.py
+++ src/server/crp/sessionPool.py
@@ -3,39 +3,51 @@ import threading
 import hashlib
 
 class SessionPool:
-    cache = SimpleCache()            # 建立session缓存
-    lock = threading.Lock()
+    __cache__ = SimpleCache()           # 建立session缓存
+    __wx2sessionId__ = SimpleCache()    # 建立wxid到sessionId的映射
+    __lock__ = threading.Lock()
     __sessionNumber__ = 0           # 服务器建立以来session的总个数(已经不存在的session+仍然存在的session)
-    md5 = hashlib.md5()             # md5生成器
+    __md5__ = hashlib.md5()             # md5生成器
 
     def __init__(self):
         pass
 
     # 生成新的session会话所需要的sessionId，并为该会话绑定wxid
     def newSession(self, wxid):
-        self.lock.acquire()
+        # 避免1个微信用户建立多个会话
+        if self.__wx2sessionId__.get(wxid):
+            raise Exception("您已登陆，请退出先前的登陆")
+        self.__lock__.acquire()
         self.__sessionNumber__+=1
-        self.lock.release()
-        self.md5.update(str(self.__sessionNumber__).encode("utf-8"))
-        sessionId = self.md5.hexdigest()
-        self.cache.set(sessionId, {"wxid":wxid})        # 为该会话创建字典
+        self.__lock__.release()
+
+        self.__md5__.update(str(self.__sessionNumber__).encode("utf-8"))
+        sessionId = self.__md5__.hexdigest()
+        self.__cache__.set(sessionId, {"__wxid__":wxid})        # 为该会话创建字典
+        self.__wx2sessionId__.set(wxid, sessionId)
         return sessionId
     
     # 删除指定的会话
     def delSession(self, sessionId):
-        self.cache.delete(sessionId)
+        self.__lock__.acquire()
+        try:
+            dic = self.__cache__.get(sessionId)
+            self.__cache__.delete(sessionId)
+            self.__wx2sessionId__.delete(dic["__wxid__"])
+        finally:
+            self.__lock__.release()
 
     def getSessionData(self, sessionId):
         dic = None
         try:
-            dic = self.cache.get(sessionId)
+            dic = self.__cache__.get(sessionId)
         finally:
             return dic
 
     def get(self, sessionId, key):
-        dic=self.cache.get(sessionId)
+        dic=self.__cache__.get(sessionId)
         return dic.get(key)
 
     def put(self, sessionId, key, val):
-        dic=self.cache.get(sessionId)
+        dic=self.__cache__.get(sessionId)
         dic[key]=val
